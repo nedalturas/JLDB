9a072c1341576388aec2fdd89d5c27d5cec75b8a
httpwanderer <nedalturas527@gmail.com>
2025-06-23 01:38:47 +0800

    implemented a 404 system

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,12 @@
 import { Button, Container, Image, SimpleGrid, Text, Title } from '@mantine/core';
-import image from '../../public/notfound.svg';
+// import image from '../assets/notfound.svg'
 import classes from './NotFound.module.css';
 
 export default function NotFound() {
   return (
     <Container className={classes.root}>
       <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
-        <Image src={image.src} className={classes.mobileImage} />
+        <Image src="https://raw.githubusercontent.com/mantinedev/ui.mantine.dev/620ed0fa88be1e484bddc711af6f0903c037f04e/lib/NotFoundImage/image.svg" className={classes.mobileImage} />
         <div>
           <Title className={classes.title}>Something is not right...</Title>
           <Text c="dimmed" size="lg">
@@ -17,7 +17,7 @@ export default function NotFound() {
             Get back to home page
           </Button>
         </div>
-        <Image src={image.src} className={classes.desktopImage} />
+        <Image src="https://raw.githubusercontent.com/mantinedev/ui.mantine.dev/620ed0fa88be1e484bddc711af6f0903c037f04e/lib/NotFoundImage/image.svg" className={classes.desktopImage} />
       </SimpleGrid>
     </Container>
   );
