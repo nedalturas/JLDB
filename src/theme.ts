7936bdac07aece89eeb022bb25753e8d67b65015
nedalturas527 <nedalturas527@gmail.com>
2025-06-23 08:28:22 +0000

    added custom theme

--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,25 @@ import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+
+  colors: {
+    primary: [
+      "#e0fbff",
+      "#cdf1ff",
+      "#a0dff9",
+      "#6fcdf3",
+      "#46bdee",
+      "#30b5ec",
+      "#12afec",
+      "#0099d2",
+      "#0088bd",
+      "#0076a8"
+    ]
+  },
+  primaryShade: 4,
+  primaryColor: "primary",
+  defaultRadius: "sm",
+  other: {
+    buttonActiveStyle: "default"
+  }
 });
